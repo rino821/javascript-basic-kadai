83dfe0a4991cedb532c1e40889d50a55cbf596c7
rino821 <matsurino@outlook.jp>
2024-09-24 00:19:08 +0900

    ファイルをアップロード

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+const today = new Date();
+
+const year = today.getFullYear() -1;
+
+const month = today.getMonth();
+
+const day = today.getDate() -2;
+
+console.log(year + '年' + month + '月' + day + '日');
